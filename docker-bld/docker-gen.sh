41827a1c89d9fc5d62a264ae7b5b7bdc6490efac
Raghavendra D Prabhu <rprabhu@wnohang.net>
2014-12-09 00:14:26 +0530

    Fixed to make it simpler and more KISS

--- docker-bld/docker-gen.sh
+++ docker-bld/docker-gen.sh
@@ -24,7 +24,7 @@ echo "
 FROM centos:centos7
 MAINTAINER Raghavendra Prabhu raghavendra.prabhu@percona.com
 RUN curl -s http://jenkins.percona.com/dev-repo/percona-dev.repo > /etc/yum.repos.d/percona-dev.repo
-RUN yum install http://epel.check-update.co.uk/7/x86_64/e/epel-release-7-2.noarch.rpm
+RUN yum install -y http://epel.check-update.co.uk/7/x86_64/e/epel-release-7-2.noarch.rpm
 RUN yum install -y http://www.percona.com/downloads/percona-release/redhat/0.1-3/percona-release-0.1-3.noarch.rpm
 RUN yum install -y which lsof libaio compat-readline5 socat percona-xtrabackup perl-DBD-MySQL perl-DBI rsync openssl098e eatmydata pv qpress gzip openssl
 RUN yum install -y bzr automake gcc  make  libtool autoconf pkgconfig gettext git scons    boost_req boost-devel libaio openssl-devel  check-devel
