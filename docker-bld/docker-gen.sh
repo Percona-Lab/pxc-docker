ace3d6600a515083535390548262a000dcf5e58b
Raghavendra D Prabhu <rprabhu@wnohang.net>
2014-12-08 23:06:18 +0530

    Fix path

--- docker-bld/docker-gen.sh
+++ docker-bld/docker-gen.sh
@@ -24,7 +24,7 @@ FROM centos:centos7
 MAINTAINER Raghavendra Prabhu raghavendra.prabhu@percona.com
 RUN curl -s http://jenkins.percona.com/dev-repo/percona-dev.repo > /etc/yum.repos.d/percona-dev.repo
 RUN yum install -y http://www.percona.com/downloads/percona-release/redhat/0.1-3/percona-release-0.1-3.noarch.rpm
-RUN yum install -y which lsof libaio compat-readline5 socat percona-xtrabackup perl-DBD-MySQL perl-DBI rsync openssl098e eatmydata pv qpress gzip
+RUN yum install -y which lsof libaio compat-readline5 socat percona-xtrabackup perl-DBD-MySQL perl-DBI rsync openssl098e eatmydata pv qpress gzip openssl
 ADD $root /pxc
 RUN mkdir -p /pxc/datadir
 ADD node.cnf /etc/my.cnf
