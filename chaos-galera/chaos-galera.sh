8dbc2ac55bbd2d4ef4826a7c1dfc6178794b4378
Raghavendra D Prabhu <rprabhu@wnohang.net>
2014-12-18 23:44:33 +0530

    Add chaos testing

--- chaos-galera/chaos-galera.sh
+++ chaos-galera/chaos-galera.sh
@@ -290,7 +290,7 @@ rm -f $HOSTSF && touch $HOSTSF
 chcon  -Rt svirt_sandbox_file_t  $HOSTSF &>/dev/null  || true
 chcon  -Rt svirt_sandbox_file_t  $COREDIR &>/dev/null  || true
 
-docker run  -d    -i -v /dev/log:/dev/log -e SST_SYSLOG_TAG=dnsmasq -v $HOSTSF:/dnsmasq.hosts --name dnscluster ronin/dnsmasq &>$LOGDIR/dnscluster-run.log
+docker run  -d  -p 20000:53   -i -v /dev/log:/dev/log -e SST_SYSLOG_TAG=dnsmasq -v $HOSTSF:/dnsmasq.hosts --name dnscluster ronin/dnsmasq &>$LOGDIR/dnscluster-run.log
 
 dnsi=$(docker inspect  dnscluster | grep IPAddress | grep -oE '[0-9\.]+')
 
