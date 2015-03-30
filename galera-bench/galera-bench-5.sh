57acc6b22d5a4446acd1a4a97952c6b216213125
Raghavendra D Prabhu <rprabhu@wnohang.net>
2015-03-30 18:56:32 +0530

    PXC-358: Add mixed testing

--- galera-bench/galera-bench-5.sh
+++ galera-bench/galera-bench-5.sh
@@ -393,14 +393,16 @@ for rest in `seq 2 $NUMC`; do
         PRELOAD=""
     fi
     if [[ $MIXED -eq 1 ]];then 
-        if [[ $(( NUMC%2 )) -eq 0 ]];then 
-            echp "Using galera3 for Dock${NUMC}"
+        if [[ $(( rest%2 )) -eq 0 ]];then 
+            echo "Using galera3 for Dock${rest}"
             PGALERA=" -v $PWD/libgalera_smm_3.so:/pxc/libgalera_smm.so -v /tmp/my.cnf:/pxc/my.cnf"
             if [[ -n ${ADDOP:-} ]];then 
                 ADDLOP="$BASEP3; $ADDOP"
             else 
                 ADDLOP="$BASEP3"
             fi
+        else 
+            echo "Using galera2 for Dock${rest}"
         fi
     fi
     docker run -P -e LD_PRELOAD=$PRELOAD  -d  -v /dev/log:/dev/log -i -e SST_SYSLOG_TAG=Dock${rest} -h Dock$rest -v $COREDIR:$icoredir $PGALERA --dns $dnsi --name Dock$rest $DIMAGE bash -c "ulimit -c unlimited && chmod 777 $icoredir && $CMD $ECMD --wsrep_cluster_address=$CSTR --wsrep_node_name=Dock$rest --wsrep-provider-options='$ADDLOP'" &>/dev/null
