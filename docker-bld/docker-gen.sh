04bcf5dfd351639760d48fa7128a5dd00ad41726
Raghavendra D Prabhu <rprabhu@wnohang.net>
2015-04-28 21:07:12 +0530

    Update the docker-bld for docker-compose

--- docker-bld/docker-gen.sh
+++ docker-bld/docker-gen.sh
@@ -7,8 +7,8 @@ branch="$1"
 
 if [[ -z $branch ]];then 
     echo "Please provide branch as first argument"
-    echo "Using lp:percona-xtradb-cluster  as default"
-    branch="lp:percona-xtradb-cluster"
+    echo "Using  master  as default"
+    branch="master"
     echo 
     echo
 fi
@@ -26,14 +26,14 @@ echo "
 FROM centos:centos7
 MAINTAINER Raghavendra Prabhu raghavendra.prabhu@percona.com
 RUN curl -s http://jenkins.percona.com/dev-repo/percona-dev.repo > /etc/yum.repos.d/percona-dev.repo
-RUN yum install -y http://epel.check-update.co.uk/7/x86_64/e/epel-release-7-2.noarch.rpm
+RUN yum install -y http://epel.check-update.co.uk/7/x86_64/e/epel-release-7-5.noarch.rpm
 RUN yum install -y http://www.percona.com/downloads/percona-release/redhat/0.1-3/percona-release-0.1-3.noarch.rpm
 RUN yum install -y which lsof libaio compat-readline5 socat percona-xtrabackup perl-DBD-MySQL perl-DBI rsync openssl098e eatmydata pv qpress gzip openssl
-RUN yum install -y bzr automake gcc  make  libtool autoconf pkgconfig gettext git scons    boost_req boost-devel libaio openssl-devel  check-devel
+RUN yum install -y bzr automake gcc  make  libtool autoconf pkgconfig gettext git scons    boost_req boost-devel libaio openssl-devel  check-devel gdb perf
 RUN yum install -y gcc-c++ gperf ncurses-devel perl readline-devel time zlib-devel libaio-devel bison cmake 
 RUN yum install -y coreutils grep procps 
-RUN bzr checkout --lightweight $branch
-WORKDIR /$(basename $branch)
+RUN git clone --depth=1 -b $branch http://github.com/percona/percona-xtradb-cluster
+WORKDIR /percona-xtradb-cluster
 RUN cmake -DBUILD_CONFIG=mysql_release -DDEBUG_EXTNAME=OFF -DWITH_ZLIB=system  -DWITH_SSL=system -DCMAKE_INSTALL_PREFIX="/usr"   .
 RUN make -j$numcp
 RUN make install
@@ -52,6 +52,6 @@ CMD  /usr/bin/mysqld --basedir=/usr --wsrep-new-cluster --user=mysql --core-file
 
 " > Dockerfile 
 
-echo "Use:> fig scale bootstrap=1 members=2 for a 3 node cluster!"
+echo "Use:> docker-compose scale bootstrap=1 members=2 for a 3 node cluster!"
 
 
